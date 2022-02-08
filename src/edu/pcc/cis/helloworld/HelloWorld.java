1a57ab90312ea3a89b1b1f4cbdf8d7ff2bd3fb52
Spencer Lankins <Spencer.Lankins@pcc.edu>
2022-02-07 21:20:56 -0800

    added main method that prints hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,10 @@
 package edu.pcc.cis.helloworld;
 
+/**
+ * Created by Spencer Lankins on 02/07/2022
+ */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
 }
